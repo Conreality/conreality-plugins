12d0cced506f7a791d60160ff04d7718cfa04355
Arto Bendiken <arto@conreality.org>
2019-07-05 18:10:37 +0300

    Added missing metadata for all current scripts.

--- conreality_scripts/lib/src/scripts.dart
+++ conreality_scripts/lib/src/scripts.dart
@@ -6,11 +6,27 @@ import 'script.dart' show Script;
 abstract class Scripts {
   Scripts._();
 
+  /// The [`developer/announce_hooks.lua`](https://github.com/conreality/conreality-scripts/blob/master/developer/announce_hooks.lua) script.
+  static Script get announceHooks => Script("developer/announce_hooks");
+
+  /// The [`developer/announce_time.lua`](https://github.com/conreality/conreality-scripts/blob/master/developer/announce_time.lua) script.
+  static Script get announceTime => Script("developer/announce_time");
+
+  /// The [`developer/flux_dosimeter.lua`](https://github.com/conreality/conreality-scripts/blob/master/developer/flux_dosimeter.lua) script.
+  static Script get fluxDosimeter => Script("developer/flux_dosimeter");
+
   /// The [`developer/keep_still.lua`](https://github.com/conreality/conreality-scripts/blob/master/developer/keep_still.lua) script.
   static Script get keepStill => Script("developer/keep_still");
 
+  /// The [`developer/spin_compass.lua`](https://github.com/conreality/conreality-scripts/blob/master/developer/spin_compass.lua) script.
+  static Script get spinCompass => Script("developer/spin_compass");
+
   /// All the scripts, indexed by their path.
   static const Map<String, Script> all = <String, Script>{
+    "developer/announce_hooks": Script("developer/announce_hooks"),
+    "developer/announce_time": Script("developer/announce_time"),
+    "developer/flux_dosimeter": Script("developer/flux_dosimeter"),
     "developer/keep_still": Script("developer/keep_still"),
+    "developer/spin_compass": Script("developer/spin_compass"),
   };
 }
